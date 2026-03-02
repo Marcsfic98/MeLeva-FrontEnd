41090e43760d5a2fe2f0e6402a6d166100c9f17d
allysonaggp <allysonaggp@gmail.com>
2026-03-02 09:06:26 -0300

    configurado sistema de rotas react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-
-
+import{BrowserRouter, Route, Routes}from "react-router-dom"
+function App() {
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
+}
 
-
-export default App
+export default App;
